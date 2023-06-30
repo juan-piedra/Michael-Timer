6a3be1082ca3fc498110f0ad28c26497ed5cb6e0
Juan Piedra <juan.piedra@rocketmail.com>
2023-06-29 21:38:16 -0700

    commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var targetDate = new Date();
+
+function updateTimer() {
+  var now = new Date();
+  var timeDiff = now.getTime() - targetDate.getTime();
+
+  var days = Math.floor(timeDiff / (1000 * 3600 * 24));
+  var hours = Math.floor((timeDiff / (1000 * 3600)) % 24);
+  var minutes = Math.floor((timeDiff / (1000 * 60)) % 60);
+  var seconds = Math.floor((timeDiff / 1000) % 60);
+
+  var timerDisplay = days + ' days, ' + hours + ' hours, ' + minutes + ' minutes, ' + seconds + ' seconds';
+  document.getElementById('timer').innerHTML = timerDisplay;
+}
+
+function resetTimer() {
+  targetDate = new Date();
+  updateTimer();
+}
+
+updateTimer();
+setInterval(updateTimer, 1000);
